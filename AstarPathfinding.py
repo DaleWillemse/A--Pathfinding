ebf83b1a1b339e5839ba4d81a548fc4047e787a5
DaleWillemse <DaleWillemse000@gmail.com>
2022-09-19 23:46:57 +0200

    Fixed - Draw grid for loop drawing twice.

--- AstarPathfinding.py
+++ AstarPathfinding.py
@@ -3,7 +3,7 @@ from turtle import pos
 import pygame
 from queue import PriorityQueue
 
-WIDTH = 1000
+WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("A* Pathfinding Algorithm")
 
